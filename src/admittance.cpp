ef00c2e60c4721529369af2829d087b0b0938c44
Iason Sarantopoulos <jasonsaras@gmail.com>
2015-06-30 20:22:23 +0300

    solve the 2nd order system

--- src/admittance.cpp
+++ src/admittance.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream> //stream class to read/write on files
 #include <boost/array.hpp>
 
 #include <boost/numeric/odeint.hpp>
@@ -6,10 +7,11 @@
 using namespace std;
 using namespace boost::numeric::odeint;
 
-const double M = 1;
+const double M = 100;
 const double B = 1;
 const double K = 2;
 double SG = 2000;
+ofstream data;
 
 typedef boost::array< double, 2 > state_type;
 
@@ -21,13 +23,18 @@ void admittance(const state_type &x, state_type &dxdt, double t)
 
 void write_adm(const state_type &x, const double t)
 {
-	cout << t << '\t' << x[0] << endl; //<< '\t' << x[1] << endl;
+	data << t << "," << x[0] << endl;
 }
 
 int main(int argc, char **argv)
 {
+
+	data.open("data.txt");
+
 	state_type x_init = {0, 0};
 	integrate(admittance, x_init, 0.0, 25.0, 0.001, write_adm);
+
+	data.close();
 }
 
 
